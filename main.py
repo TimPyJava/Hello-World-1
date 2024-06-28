b89360d36485657484d1936adb3b485b8a7b9635
Timothy <163576499+TimPyJava@users.noreply.github.com>
2024-06-28 19:42:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Timothy made this program. The purpose of this program is to
+# create the "Hello World" program. There is no input. The out
+# put is the phrase "Hello World".
+
+print("Hello World.")
